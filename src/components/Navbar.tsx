28f88648991cefc8defd5dbb5fdfbd328c20349c
Kazal Oshodi <kazoshodi@gmail.com>
2024-12-24 21:13:55 +0100

    more changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,39 @@
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { quotes } from "@/utils/Quotes";
+import { Box, Float, Heading, HStack, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { Blockquote, BlockquoteIcon } from "./ui/blockquote";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
+  const index = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[index];
+
   return (
     <header>
-      <HStack bg="gray.100" px="6" py="4" boxShadow="sm">
+      <HStack
+        bg="gray.100"
+        px="6"
+        py="4"
+        boxShadow="sm"
+        justifyContent="space-between"
+      >
         <Heading>Baking Scaler</Heading>
-        <Box ml="auto">
+        <VStack flex="1" align="center">
+          <Blockquote
+            width="50vw"
+            variant="plain"
+            colorPalette="red"
+            showDash
+            icon={
+              <Float placement="top-start" offsetY="2">
+                <BlockquoteIcon />
+              </Float>
+            }
+          >
+            {quote}
+          </Blockquote>
+        </VStack>
+        <Box>
           <Button mr="4">
             <Link to="/">Home Page</Link>
           </Button>
