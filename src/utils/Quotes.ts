ed4ae9f7f3e280a065faab680fc5f921663d3ca9
Kazal Oshodi <kazoshodi@gmail.com>
2024-12-24 21:14:04 +0100

    more changes

---
+++ src/utils/Quotes.ts
@@ -0,0 +1,23 @@
+export const quotes: string[] = [
+	"Baking is love made edible.",
+	"Happiness is homemade.",
+	"Bake till you make it!",
+	"The secret ingredient is always love.",
+	"You can't buy happiness, but you can bake it.",
+	"Cookies make everything better.",
+	"Baking is my love language.",
+	"Let love rise, like a perfect loaf in the oven.",
+	"A recipe has no soul - you bring the love to make it special.",
+	"Bake what you love, love what you bake.",
+	"Sometimes, all you need is a good friend and a fresh batch.",
+	"When words aren't enough, bake something special.",
+	"A bad day spent baking is better than a good day doing anything else.",
+	"Baking is cheaper than therapy.",
+	"Baking is love made visible.",
+	"Never underestimate a woman who loves baking.",
+	"I believe with baking there is a certain nostalgic fix.",
+	"Baking is Life. Do you see? It's not just recipes.",
+	"Having a mental bake-down.",
+	"I love baking, it's the most calming thing for me. It's therapeutic, it makes the house smell good, and I get to take the goods to my friends. I do it for other people."
+      ];
+      
